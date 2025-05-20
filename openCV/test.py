44e680859b0d30f1331ef9bccc8b9ff415b736e9
Jove <jovefong62@gmail.com>
2025-05-20 17:08:59 +0700

    Implemented handtracking algorithm with mediapipe

--- openCV/test.py
+++ openCV/test.py
@@ -1,29 +1,35 @@
-from tkinter import W
-import numpy as np, sys, cv2
+import numpy as np, sys, cv2, mediapipe as mp
 
 img = cv2.imread("sample.jpg")
 cv2.imshow('idPhotoImage', img)
-print("Shape:", img.shape)       # (H, W, 3)
-print("Data type:", img.dtype)   # usually uint8
-print("Pixel value at (100, 100):", img[100, 100])  # [B, G, R]
+# print("Shape:", img.shape)       # (H, W, 3)
+# print("Data type:", img.dtype)   # usually uint8
+# print("Pixel value at (100, 100):", img[100, 100])  # [B, G, R]
 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # array of unsigned int, calculated using a formula from its RGB values
 blur = cv2.GaussianBlur(img, (5, 5), 10) # array of unsigned int
-cv2.imshow("Grayscale", gray)
-cv2.imshow("Blurred", blur)
+# cv2.imshow("Grayscale", gray)
+# cv2.imshow("Blurred", blur)
 
 # Resizing
 roi = img[100:200, 200:400]
-cv2.imshow("roi", roi)
+# cv2.imshow("roi", roi)
 
 # Apply binary threshold: convert to pure black and white
-_, thresh = cv2.threshold(img, 127, 255, cv2.THRESH_TOZERO)
-cv2.imshow("Thresholded", thresh)
+_, thresh = cv2.threshold(img, 255, 255, cv2.THRESH_TOZERO)
+# cv2.imshow("Thresholded", thresh)
 
 # masking
-mask = gray > 100 # array of boolean (T if > 100)
+mask = gray > 150 # array of boolean (T if > 100)
 masked = np.zeros_like(img) # array of uint8
 masked[mask] = img[mask] # copies pixel values from img into masked if mask = true
+cv2.imshow("masked", masked)
+
+# drawings to debug
+cv2.rectangle(img, (100, 100), (200, 200), (0, 255, 0), 2)
+cv2.line(img, (0, 300), (400, 300), (255, 0, 0), 2)
+cv2.circle(img, (250, 250), 40, (0, 0, 255), 3)
+# cv2.imshow("Drawn", img)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
---
+++ openCV/videoProcessing.py
@@ -0,0 +1,64 @@
+import numpy as np, cv2, mediapipe as mp
+cap = cv2.VideoCapture("./WorkingGuy.mp4")
+width  = 320
+height = 515
+startLine = 235
+endLine = 20
+state = "start"
+tolerance = 5
+count = 0
+
+mp_drawing = mp.solutions.drawing_utils
+mp_hands = mp.solutions.hands
+hand = mp_hands.Hands()
+
+if not cap.isOpened():
+  print("Error opening video")
+  exit(0)
+
+while True:
+  ret, frame = cap.read()
+  if not ret:
+    print("cannot read frame")
+    break
+
+  desiredFrame = frame[400:, 300:815] # given in BGR
+  result = hand.process(cv2.cvtColor(desiredFrame, cv2.COLOR_BGR2RGB)) # convert to RGB to use
+
+  # Checking for hands
+  if result.multi_hand_landmarks:
+    for hand_landmarks in result.multi_hand_landmarks:
+      wrist = hand_landmarks.landmark[0]
+      h, w, _ = desiredFrame.shape
+      # landmark_x_pixel = int(wrist.x * h)
+      landmark_y_pixel = int(wrist.y * h)
+      # print(h)
+      # print(landmark_y_pixel)
+
+      if state == "start":
+        if abs(landmark_y_pixel - startLine) <= tolerance:
+          state = "waiting to end"
+          print("Passed start")
+
+      elif state == "waiting to end":
+        if abs(landmark_y_pixel - endLine) <= tolerance:
+            state = "counted"
+            count += 1
+            print(f"Passed endLine → Counted! Total: {count}")
+      
+      elif state == "counted":
+        if landmark_y_pixel > startLine + tolerance:
+          state = "start"
+          print("restart")
+
+      mp_drawing.draw_landmarks(desiredFrame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+  
+  # gray = cv2.cvtColor(desiredFrame, cv2.COLOR_BGR2GRAY)
+  cv2.line(desiredFrame, (0, 250), (815, 250), (0, 255, 0), 2) # green line as start point
+  cv2.line(desiredFrame, (0, 20), (815, 20), (0, 0, 255), 2) # red line as end point
+  cv2.imshow('frame', desiredFrame) 
+  if cv2.waitKey(10) == ord('q'):
+    break
+
+cap.release()
+cv2.destroyAllWindows()
