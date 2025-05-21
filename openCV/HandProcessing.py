52014438fa54875d3611032164a347abe2894104
Jove <jovefong62@gmail.com>
2025-05-21 17:19:14 +0700

    Trialed HandProcessing by Google

--- openCV/HandProcessing.py
+++ openCV/HandProcessing.py
@@ -3,11 +3,11 @@ from mediapipe import solutions
 from mediapipe.framework.formats import landmark_pb2
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
-import numpy as np
+import numpy as np 
 import cv2
 
 # Constants
-MARGIN = 10  # pixels
+MARGIN = 9  # pixels
 FONT_SIZE = 1
 FONT_THICKNESS = 1
 HANDEDNESS_TEXT_COLOR = (88, 205, 54)  # vibrant green
